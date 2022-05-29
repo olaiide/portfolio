d199d8898f4c2abc034cd8f8e908e975912bb100
olaiide <horlaide909@gmail.com>
2022-05-28 19:31:59 -1200

    form changes

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,31 +4,29 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <Fragment>
-      <div className="container-contact" id="Contact">
-       
-        <div className="contact">
-      
+      <div className='container-contact' id='Contact'>
+        <div className='contact'>
           <div>
-          <h3>Contact Me</h3>
-        <p>Got a project or an idea? Get in touch.</p>
+            <h3>Contact Me</h3>
+            <p className="got">Got a project or an idea? Get in touch.</p>
             <Form />
           </div>
-          <div className="main-contact">
-            <div className="address">
+          <div className='main-contact'>
+            <div className='address'>
               <span>
-                <i class="fa fa-map-marker" aria-hidden="true"></i>Lagos
+                <i class='fa fa-map-marker' aria-hidden='true'></i>Lagos
               </span>
             </div>
-            <div className="mail">
+            <div className='mail'>
               <span>
-                <i class="far fa-envelope-open"></i>
-                <a href="mailto:horlaide909@gmail.com">horlaide909@gmail.com</a>
+                <i class='far fa-envelope-open'></i>
+                <a href='mailto:horlaide909@gmail.com'>horlaide909@gmail.com</a>
               </span>
             </div>
-            <div className="telephone">
+            <div className='telephone'>
               <span>
-                <i class="fas fa-phone"></i>
-                <a href="tel:+234-903-611-7357">+234-903-611-7357</a>
+                <i class='fas fa-phone'></i>
+                <a href='tel:+234-903-611-7357'>+234-903-611-7357</a>
               </span>
             </div>
           </div>
